a7af551b2f8d44a8dfee854a9d43773860811a18
Nikhil Chadha <58354293+nikhil697@users.noreply.github.com>
2024-04-06 14:51:30 +0530

    Time: 18 ms (80.14%) | Memory: 12.3 MB (74.21%) - LeetSync

---
+++ 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    string minRemoveToMakeValid(string s) {
+        int n = s.size();
+        char ch[n + 1];
+        strcpy(ch, s.c_str());
+
+        string result="";
+        int count=0;
+        // For Left to right
+        for(int i=0;i<n;i++){
+            if(ch[i]=='('){
+                count++;
+            }
+            else if(ch[i]==')'){
+                if(count>0){
+                    count--;
+                }
+                else{
+                    ch[i]=0;
+                }
+            }
+        }
+        count=0;
+        // For right to left
+        for(int i=n-1;i>=0;i--){
+            if(ch[i]==')'){
+                count++;
+            }
+            else if(ch[i]=='('){
+                if(count>0){
+                    count--;
+                }
+                else{
+                    ch[i]=0;
+                }
+            }
+        }
+        
+        for(char c:ch){
+            if(c!=0){
+                result.push_back(c);
+            }
+        }
+        return result;
+        
+        
+        
+    }
+};
