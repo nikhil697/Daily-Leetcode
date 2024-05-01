e799a440312bcb85f6bc0b055795c774d2e1b6b4
Nikhil Chadha <58354293+nikhil697@users.noreply.github.com>
2024-05-02 00:48:04 +0530

    Time: 3 ms (42.21%) | Memory: 7.5 MB (65.62%) - LeetSync

--- 2128-reverse-prefix-of-word/reverse-prefix-of-word.cpp
+++ 2128-reverse-prefix-of-word/reverse-prefix-of-word.cpp
@@ -4,11 +4,11 @@ public:
         int index=0;
         for(int i=0;i<word.size();i++){
             if(word[i]==ch){
-                index=i+1;
+                index=i;
                 break;
             }
         }
-        reverse(word.begin(),word.begin()+index);
+        reverse(word.begin(),word.begin()+index+1);
         return word;
         
     }
