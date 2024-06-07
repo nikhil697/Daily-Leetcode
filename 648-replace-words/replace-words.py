77f1457ad60b032d5e6ce56550f41981ee868eeb
Nikhil Chadha <58354293+nikhil697@users.noreply.github.com>
2024-06-07 21:38:52 +0530

    Time: 353 ms (13.56%) | Memory: 24.9 MB (81.26%) - LeetSync

--- 648-replace-words/replace-words.py
+++ 648-replace-words/replace-words.py
@@ -5,15 +5,12 @@ class Solution:
         dict_set = set(dictionary)
 
         def shortest_root(word, dict_set):
-            # Find the shortest root of the word in the dictionary
             for i in range(len(word)):
                 root = word[0:i]
                 if root in dict_set:
                     return root
-            # There is not a corresponding root in the dictionary
+        
             return word
-
-        # Replace each word in sentence with the corresponding shortest root
         for word in range(len(word_array)):
             word_array[word] = shortest_root(word_array[word], dict_set)
 
