a7c3962e72802cad12676a6a13bc4a85b55696b2
Nikhil Chadha <58354293+nikhil697@users.noreply.github.com>
2024-09-04 23:57:37 +0530

    Time: 19 ms (85.37%) | Memory: 29.5 MB (35.42%) - LeetSync

--- 222-count-complete-tree-nodes/count-complete-tree-nodes.cpp
+++ 222-count-complete-tree-nodes/count-complete-tree-nodes.cpp
@@ -1,32 +1,24 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
     int countNodes(TreeNode* root) {
-        queue<TreeNode*>q;
-        if(root==nullptr){
-            return 0;
-        }
-        q.push(root);
-        int count=0;
-        TreeNode* temp;
-        while(!q.empty()){
-            temp=q.front();
-            q.pop();
-            count+=1;
-            if(temp->left) q.push(temp->left);
-            if(temp->right) q.push(temp->right);
-        }
-        return count;
+    //     queue<TreeNode*>q;
+    //     if(root==nullptr){
+    //         return 0;
+    //     }
+    //     q.push(root);
+    //     int count=0;
+    //     TreeNode* temp;
+    //     while(!q.empty()){
+    //         temp=q.front();
+    //         q.pop();
+    //         count+=1;
+    //         if(temp->left) q.push(temp->left);
+    //         if(temp->right) q.push(temp->right);
+    //     }
+    //     return count;
+    // }
+        if(!root) return 0;
+        return(1+countNodes(root->left)+countNodes(root->right));
     }
     
 };
