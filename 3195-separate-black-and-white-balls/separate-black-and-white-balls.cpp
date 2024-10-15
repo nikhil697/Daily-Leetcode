46ebd47eb1962cb3b91101c911cf7f64aa894b0f
Nikhil Chadha <58354293+nikhil697@users.noreply.github.com>
2024-10-15 17:21:52 +0530

    Time: 35 ms (43.16%) | Memory: 15.6 MB (76.42%) - LeetSync

--- 3195-separate-black-and-white-balls/separate-black-and-white-balls.cpp
+++ 3195-separate-black-and-white-balls/separate-black-and-white-balls.cpp
@@ -4,11 +4,11 @@ public:
         long long steps=0;
         long long counter=0;
         for(int j=s.length()-1;j>=0;j--){
-            if(s[j]=='1'){
-                steps+=counter;
+            if(s[j]=='0'){
+                counter++;
             }
-            else{
-                counter++;               
+            else if(s[j]=='1'){
+                steps+=counter;
             }
         }
         return steps;
