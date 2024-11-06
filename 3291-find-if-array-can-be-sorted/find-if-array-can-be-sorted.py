a4b0641a3a58e7d0c8ab29913b35404ca4358398
Nikhil Chadha <58354293+nikhil697@users.noreply.github.com>
2024-11-07 00:54:33 +0530

    Time: 7 ms (78.18%) | Memory: 16.8 MB (23.35%) - LeetSync

---
+++ 3291-find-if-array-can-be-sorted/find-if-array-can-be-sorted.py
@@ -0,0 +1,34 @@
+class Solution:
+    def canSortArray(self, nums):
+        # Number of set bits of the elements in the current segment
+        num_of_set_bits = bin(nums[0]).count("1")
+        max_of_segment = nums[0]
+        min_of_segment = nums[0]
+
+        # Initialize max of the previous segment to the smallest possible integer
+        max_of_prev_segment = float("-inf")
+
+        for i in range(1, len(nums)):
+            if bin(nums[i]).count("1") == num_of_set_bits:
+                # Element belongs to the same segment
+                # Update min and max values of the segment
+                max_of_segment = max(max_of_segment, nums[i])
+                min_of_segment = min(min_of_segment, nums[i])
+            else:  # Element belongs to a new segment
+                # Check if the segments are arranged properly
+                if min_of_segment < max_of_prev_segment:
+                    return False
+
+                # Update the previous segment's max
+                max_of_prev_segment = max_of_segment
+
+                # Start a new segment with the current element
+                max_of_segment = nums[i]
+                min_of_segment = nums[i]
+                num_of_set_bits = bin(nums[i]).count("1")
+
+        # Final check for proper segment arrangement
+        if min_of_segment < max_of_prev_segment:
+            return False
+
+        return True
